8e30093595aaf6f4cacf02b57346ef7c927cd0a2
Ana Nikolova <ani.nik19@gmail.com>
2019-07-18 10:25:32 +0200

    Route Transitions

--- color/src/NewPaletteForm.js
+++ color/src/NewPaletteForm.js
@@ -177,6 +177,7 @@ class NewPaletteForm extends Component {
             removeColor={this.removeColor}
             axis="xy"
             onSortEnd={this.onSortEnd}
+            distance={20}
           />
         </main>
       </div>
